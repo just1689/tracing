21ac2672e3b417f43adb283f01c2bb1bdb9cafd7
Just1689 <Just1689@gmail.com>
2019-09-04 17:59:50 +0200

    init

---
+++ model.go
@@ -0,0 +1,70 @@
+package tracing
+
+import (
+	"encoding/hex"
+	"math/rand"
+	"time"
+)
+
+func NewSpan(traceID string, serviceName, rpcName string, d time.Duration) Span {
+	result := Span{
+		ID:     newId(),
+		Name:   rpcName,
+		Shared: true,
+		LocalEndpoint: map[string]string{
+			"serviceName": serviceName,
+		},
+		TraceID: traceID,
+	}
+	result.SetDuration(d)
+	result.SetTimestampNow()
+	return result
+}
+
+func NewSpanChild(traceID string, parentID, serviceName, rpcName string, d time.Duration) Span {
+	result := NewSpan(traceID, serviceName, rpcName, d)
+	result.ParentID = parentID
+	return result
+}
+
+type Span struct {
+	Duration      int64             `json:"duration,omitempty"`
+	ID            string            `json:"id"`
+	Name          string            `json:"name,omitempty"`
+	ParentID      string            `json:"parentId,omitempty"`
+	Shared        bool              `json:"shared,omitempty"`
+	LocalEndpoint map[string]string `json:"localEndpoint,omitempty"`
+	Tags          Tags              `json:"tags,omitempty"`
+	Timestamp     int64             `json:"timestamp,omitempty"`
+	TraceID       string            `json:"traceId"`
+}
+
+func (s *Span) SetDuration(d time.Duration) {
+	s.Duration = d.Nanoseconds() / 1000
+}
+
+func (s *Span) SetTimestamp(t time.Time) {
+	s.Timestamp = t.UnixNano() / 1000
+}
+
+func (s *Span) SetTimestampNow() {
+	s.SetTimestamp(time.Now())
+}
+
+type Tags map[string]string
+
+const traceIDLen = 16
+
+func newId() string {
+	return randString(traceIDLen)
+}
+
+var src = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func randString(n int) string {
+	b := make([]byte, (n+1)/2)
+	if _, err := src.Read(b); err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(b)[:n]
+}
---
+++ tracing.go
@@ -0,0 +1,98 @@
+package tracing
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var GlobalPublisher *publisher
+
+type Config struct {
+	Url             string //http://localhost:9411/api/v2/spans
+	CacheSize       int
+	FlushTimeout    int
+	FlushSize       int
+	SleepBetweenErr int
+	RetryErr        bool
+}
+
+func StartTracing(c Config) {
+	p := &publisher{
+		url:             c.Url,
+		in:              make(chan Span, c.CacheSize),
+		flushTimeout:    time.Duration(c.FlushTimeout) * time.Second,
+		flushSize:       c.FlushSize,
+		SleepBetweenErr: c.SleepBetweenErr,
+		RetryErr:        c.RetryErr,
+	}
+	GlobalPublisher = p
+	go p.run()
+	return
+}
+
+type publisher struct {
+	url             string
+	in              chan Span
+	flushTimeout    time.Duration
+	flushSize       int
+	SleepBetweenErr int
+	RetryErr        bool
+}
+
+func (p *publisher) Enqueue(s Span) {
+	p.in <- s
+}
+func (p *publisher) EnqueueAll(all []Span) {
+	for _, s := range all {
+		p.in <- s
+	}
+}
+func (p *publisher) run() {
+	var arr = make([]Span, 0)
+	go func() {
+		start := time.Now()
+		for {
+			select {
+			case s := <-p.in:
+				arr = append(arr, s)
+				if len(arr) > p.flushSize || time.Since(start) > p.flushTimeout {
+					p.sendNow(arr)
+					arr = make([]Span, 0)
+				}
+			case <-time.After(p.flushTimeout):
+				if len(arr) > 0 {
+					p.sendNow(arr)
+					arr = make([]Span, 0)
+				}
+			}
+		}
+	}()
+}
+
+func (p *publisher) sendNow(s []Span) {
+	b, err := json.Marshal(s)
+	if err != nil {
+		logrus.Errorln(err)
+		return
+	}
+	r := bytes.NewReader(b)
+	resp, err := http.Post(p.url, "Application/json", r)
+	if err != nil || resp.StatusCode < 200 || resp.StatusCode > 299 {
+		logrus.Errorln(resp.StatusCode, err)
+		b, err = ioutil.ReadAll(resp.Body)
+		fmt.Println(string(b))
+		if p.RetryErr {
+			p.EnqueueAll(s)
+		}
+		if p.SleepBetweenErr != 0 {
+			time.Sleep(time.Duration(p.SleepBetweenErr) * time.Second)
+		}
+		return
+	}
+
+}

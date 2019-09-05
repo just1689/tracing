5b133210de80fa9f0764d2c725ee1ceef9f3bbd3
Just1689 <Just1689@gmail.com>
2019-09-05 17:38:03 +0200

    clean up

--- tracing.go
+++ tracing.go
@@ -55,24 +55,22 @@ func (p *publisher) EnqueueAll(all []Span) {
 func (p *publisher) run() {
 	var arr = make([]Span, 0)
 	lastSend := time.Now()
+	var readyToSend bool
 	for {
 		select {
 		case s := <-p.in:
 			arr = append(arr, s)
-			if len(arr) >= p.flushSize || time.Since(lastSend) > p.flushTimeout {
-				p.sendNow(arr)
-				arr = make([]Span, 0)
-				lastSend = time.Now()
-			}
+			readyToSend = len(arr) >= p.flushSize || time.Since(lastSend) >= p.flushTimeout
 		case <-time.After(p.flushTimeout):
-			if len(arr) > 0 {
-				p.sendNow(arr)
-				arr = make([]Span, 0)
-				lastSend = time.Now()
-			}
+			readyToSend = len(arr) > 0
+		}
+
+		if readyToSend {
+			p.sendNow(arr)
+			arr = make([]Span, 0)
+			lastSend = time.Now()
 		}
 	}
-
 }
 
 func (p *publisher) sendNow(s []Span) {

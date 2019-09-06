d75287b8d7d9d976b9619ea1b9b7707026a1b214
Just1689 <Just1689@gmail.com>
2019-09-06 08:10:36 +0200

    refactor for concurrency

--- tracing.go
+++ tracing.go
@@ -9,36 +9,49 @@ import (
 
 var GlobalPublisher *publisher
 
+func NewDefaultConfig() Config {
+	return Config{
+		Url:             "http://localhost:9411/api/v2/spans",
+		CacheSize:       1024,
+		FlushTimeout:    5,
+		FlushSize:       25,
+		SleepBetweenErr: 1,
+	}
+}
+
 type Config struct {
-	Url             string //http://localhost:9411/api/v2/spans
+	Url             string
 	CacheSize       int
 	FlushTimeout    int
 	FlushSize       int
 	SleepBetweenErr int
-	RetryErr        bool
 }
 
 func StartTracing(c Config) {
 	p := &publisher{
 		url:             c.Url,
 		in:              make(chan Span, c.CacheSize),
+		collected:       make(chan []Span),
+		marshaled:       make(chan []byte),
 		flushTimeout:    time.Duration(c.FlushTimeout) * time.Second,
 		flushSize:       c.FlushSize,
 		SleepBetweenErr: c.SleepBetweenErr,
-		RetryErr:        c.RetryErr,
 	}
 	GlobalPublisher = p
-	go p.run()
+	go p.collect()
+	go p.marshal()
+	go p.send()
 	return
 }
 
 type publisher struct {
 	url             string
 	in              chan Span
+	collected       chan []Span
+	marshaled       chan []byte
 	flushTimeout    time.Duration
 	flushSize       int
 	SleepBetweenErr int
-	RetryErr        bool
 }
 
 func (p *publisher) Enqueue(s Span) {
@@ -49,7 +62,7 @@ func (p *publisher) EnqueueAll(all []Span) {
 		p.in <- s
 	}
 }
-func (p *publisher) run() {
+func (p *publisher) collect() {
 	var arr = make([]Span, 0)
 	lastSend := time.Now()
 	var readyToSend bool
@@ -63,28 +76,37 @@ func (p *publisher) run() {
 		}
 
 		if readyToSend {
-			p.postSpans(arr)
+			p.collected <- arr
 			arr = make([]Span, 0)
 			lastSend = time.Now()
 		}
 	}
 }
 
-func (p *publisher) postSpans(s []Span) {
-	b, err := json.Marshal(s)
-	if err != nil {
-		logrus.Errorln(err)
-		return
-	}
-	r := bytes.NewReader(b)
-	err = httpPostJson(p.url, r)
-	if err != nil {
-		if p.RetryErr {
-			p.EnqueueAll(s)
+func (p *publisher) marshal() {
+	var err error
+	var b []byte
+	for s := range p.collected {
+		b, err = json.Marshal(s)
+		if err != nil {
+			logrus.Errorln(err)
+			continue
 		}
-		if p.SleepBetweenErr != 0 {
-			time.Sleep(time.Duration(p.SleepBetweenErr) * time.Second)
-		}
+		p.marshaled <- b
 	}
+}
 
+func (p *publisher) send() {
+	var b []byte
+	var err error
+	for b = range p.marshaled {
+		r := bytes.NewReader(b)
+		err = httpPostJson(p.url, r)
+		if err != nil {
+			if p.SleepBetweenErr != 0 {
+				time.Sleep(time.Duration(p.SleepBetweenErr) * time.Second)
+			}
+		}
+
+	}
 }
